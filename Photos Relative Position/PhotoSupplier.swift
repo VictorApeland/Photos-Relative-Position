eeac521590f785d8991a805d66e855e8effbbcbd
Victor Apeland <victor.apeland@icloud.com>
2019-05-06 20:18:41 +0200

    Adding ignore and attributes

---
+++ Photos Relative Position/PhotoSupplier.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSupplier.swift
+//  Photos Relative Position
+//
+//  Created by Victor Apeland on 30/04/2019.
+//  Copyright © 2019 Victor Apeland. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+
+func getRandomPhotos () -> [UIImage] {
+	let imageManager = PHImageManager.default()
+	return [#imageLiteral(resourceName: "BlackHole")]
+}
+
